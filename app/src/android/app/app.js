cc277700cedbf168f1cea28fc68a6cc590f59a95
Eduard Tkachuk <coolworld@MacBook-Air-Eduard.local>
2019-05-22 17:11:12 +0300

    init

---
+++ app/src/android/app/app.js
@@ -0,0 +1,72 @@
+'use strict';
+
+import React, {Component} from 'react';
+import {
+    BackHandler
+} from 'react-native';
+
+console.disableYellowBox = true;
+
+import Login from './login';
+import AppContainer from './appContainer';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        BackHandler.addEventListener('hardwareBackPress', () => {
+			if (this.props.navigator) {
+				this.props.navigator.pop();
+			}
+			return true;
+		});
+
+        this.state = {
+            isLoggedIn: false
+        };
+
+        window.appConfig = {
+            access_token: '',
+            url: 'http://jwt-base.herokuapp.com/',
+			onLogOut: this.onLogOut.bind(this),
+			balance: {
+                refresh: false
+            },
+            outputs: {
+                refresh: false
+            },
+            contacts: {
+                refresh: false
+            },
+
+            users: {
+                refresh: false
+            },
+            phones: {
+                refresh: false
+            }
+        };
+    }
+
+    render() {
+        if (this.state.isLoggedIn) {
+            return (
+                <AppContainer />
+            )
+        } else {
+            return (
+                <Login onLogin={this.onLogin.bind(this)}/>
+            )
+        }
+    }
+
+    onLogin() {
+        this.setState({isLoggedIn: true});
+    }
+
+    onLogOut() {
+        this.setState({isLoggedIn: false});
+    }
+}
+
+export default App;
