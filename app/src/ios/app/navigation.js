aef60e5aa1707093072bdc9d0866e7051314c0c8
coolworld2015 <wintermute2015@ukr.net>
2019-06-15 16:04:23 +0300

    iOS - added phones

--- app/src/ios/app/navigation.js
+++ app/src/ios/app/navigation.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import { createBottomTabNavigator, createStackNavigator, createAppContainer } from 'react-navigation';
 
+import Phones from '../phones/phones';
+import PhoneDetails from '../phones/phoneDetails';
+
+import Search from '../search/search';
+import SearchResults from '../search/searchResults';
+
 import Users from '../users/users';
 import UserDetails from '../users/userDetails';
 import UserAdd from '../users/userAdd';
 
 import Audit from '../audit/audit';
 import AuditDetails from '../audit/auditDetails';
+
+const PhonesTab = createStackNavigator({
+  Phones,
+  PhoneDetails,
+  Search,
+  SearchResults
+});
 
 const UsersTab = createStackNavigator({
   Users,
@@ -27,6 +40,7 @@ class Quit extends React.Component {
 }
 
 const TabNavigator = createBottomTabNavigator({
+  Phones: PhonesTab,
   Users: UsersTab,
   Audit: AuditTab,
   Quit: Quit
